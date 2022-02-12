8487b7cf314cae7af271cb74ad159d86f9a66890
Basix <basix@basix.tech>
2022-02-12 22:47:08 +0900

    Improve JSX support

--- src/entrypoints/MonacoEditor.tsx
+++ src/entrypoints/MonacoEditor.tsx
@@ -1,8 +1,8 @@
-import Editor, { OnChange } from "@monaco-editor/react";
+import Editor, { OnChange, useMonaco } from "@monaco-editor/react";
 import get from "lodash/get";
 import { RenderFieldExtensionCtx } from "datocms-plugin-sdk";
 import { Canvas } from "datocms-react-ui";
-import { useCallback } from "react";
+import { useCallback, useEffect } from "react";
 import { Parameters } from "./MonacoConfig";
 
 interface MonacoEditorProps {
@@ -12,12 +12,23 @@ interface MonacoEditorProps {
 export default function MonacoEditor({ ctx }: MonacoEditorProps) {
   const parameters: Parameters = ctx.parameters as unknown as Parameters;
   const language =
-    (get(ctx.formValues, ctx.fieldPath.replace(/\w+$/, parameters.languageField.value)) as string) ?? "text";
+    (get(
+      ctx.formValues,
+      ctx.fieldPath.replace(/\w+$/, parameters.languageField.value)
+    ) as string) ?? "text";
   const onChange = useCallback<OnChange>(
     (v) => v && ctx.setFieldValue(ctx.fieldPath, v),
     [ctx]
   );
   const value = get(ctx.formValues, ctx.fieldPath, "") as string;
+
+  const monaco = useMonaco();
+  useEffect(() => {
+    if (!monaco) return;
+    monaco.languages.typescript.javascriptDefaults.setCompilerOptions({
+      jsx: 1,
+    });
+  }, [monaco]);
 
   return (
     <Canvas ctx={ctx}>
